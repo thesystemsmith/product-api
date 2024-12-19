1a38b62c685d21a207d0f9217e0874c31e82c4c5
thesystemsmith <abdulrasheed.techy@gmail.com>
2024-12-19 23:05:51 +0530

    data schema defined

---
+++ src/main/java/com/example/product_api/model/Product.java
@@ -0,0 +1,18 @@
+package com.example.product_api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
+
+}
