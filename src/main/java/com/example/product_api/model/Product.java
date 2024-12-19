b50c10479132b50116e33937f466d6131e4396e4
thesystemsmith <abdulrasheed.techy@gmail.com>
2024-12-19 23:35:31 +0530

    manually created getters and setters instead of lombok @Data

--- src/main/java/com/example/product_api/model/Product.java
+++ src/main/java/com/example/product_api/model/Product.java
@@ -1,9 +1,9 @@
 package com.example.product_api.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+//import lombok.Data;
 
-@Data
+//@Data
 @Entity
 @Table(name = "products")
 public class Product {
@@ -14,5 +14,29 @@ public class Product {
     private String name;
     private String description;
     private Double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 
 }
